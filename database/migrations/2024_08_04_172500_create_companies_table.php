d337d7c760a5d4363d78dae7f845752dfccea956
Fernanda <sanjaya.dev20@gmail.com>
2024-08-06 09:05:16 +0700

    update 6 agustus

--- database/migrations/2024_08_04_172500_create_companies_table.php
+++ database/migrations/2024_08_04_172500_create_companies_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('logo')->nullable();
+            $table->bigInteger('user_id')->unsigned();
+            
             $table->softDeletes();
             $table->timestamps();
         });
