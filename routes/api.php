d5f717d00cfdb06ccc11c3a17c546d42b33ced4c
Fernanda <sanjaya.dev20@gmail.com>
2024-08-14 12:15:28 +0700

    update api

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
-use App\Http\Controllers\API\CompanyController;
-use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\RoleController;
+use App\Http\Controllers\API\TeamController;
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\CompanyController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,12 +23,29 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
+// company api
 Route::prefix('company')->middleware('auth:sanctum')->name('company.')->group( function () {
         Route::get('',[CompanyController::class,'fetch'])->name('fetch');
         Route::post('',[CompanyController::class,'create'])->name('create');
         Route::post('update/{id}',[CompanyController::class,'update'])->name('update');
-});
-
+    });
+    
+// team api
+Route::prefix('team')->middleware('auth:sanctum')->name('team.')->group( function () {
+        Route::get('',[TeamController::class,'fetch'])->name('fetch');
+        Route::post('',[TeamController::class,'create'])->name('create');
+        Route::post('update/{id}',[TeamController::class,'update'])->name('update');
+        Route::delete('{id}',[TeamController::class,'destroy'])->name('delete');
+    });
+// role api
+Route::prefix('role')->middleware('auth:sanctum')->name('role.')->group( function () {
+        Route::get('',[RoleController::class,'fetch'])->name('fetch');
+        Route::post('',[RoleController::class,'create'])->name('create');
+        Route::post('update/{id}',[RoleController::class,'update'])->name('update');
+        Route::delete('{id}',[RoleController::class,'destroy'])->name('delete');
+    });
+    
+// auth api
 Route::name('auth.')->group(function () {
     Route::post('/login',[UserController::class, 'login'])->name('login');
     Route::post('/register',[UserController::class, 'register'])->name('register');
